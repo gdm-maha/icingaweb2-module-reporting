3ab3f0f7ca9abfbcb39751876efc15c845b8792f
gdm-maha <159897970+gdm-maha@users.noreply.github.com>
2026-09-14 07:47:02 +0200

    Enhance documentation for execute method
    Updated docblock for the execute method to clarify asynchronous behavior.

--- library/Reporting/Hook/ActionHook.php
+++ library/Reporting/Hook/ActionHook.php
@@ -17,8 +17,16 @@ abstract class ActionHook
     abstract public function getName();
 
     /**
+     * Execute this action
+     *
+     * If asynchronous work needs to be awaited (e.g. exporting a PDF), return the
+     * corresponding {@see \React\Promise\PromiseInterface} instead of blocking, since this
+     * may be called from within an already running event loop (e.g. the scheduler daemon).
+     *
      * @param Report $report
      * @param array  $config
+     *
+     * @return \React\Promise\PromiseInterface|null
      */
     abstract public function execute(Report $report, array $config);
 
